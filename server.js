ae0ad5fca8a420094f0bf1edf9e5f5411b32fa52
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-04-28 18:58:09 -0700

    created server

---
+++ server.js
@@ -0,0 +1,28 @@
+// require express 
+const express = require('express');
+// create a port for our server
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Express middleware
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Testing testing hello testing',
+    });
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
