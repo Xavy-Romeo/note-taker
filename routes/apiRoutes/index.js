f71ee74a06c1222edb8b76dcf56b7528ecdb5ea8
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-04-29 00:35:42 -0700

    posts pushed db.json

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const {findById, createNewNote} = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 // get all notes route
@@ -9,21 +10,25 @@ router.get('/notes', (req, res) => {
 
 // get note by id route
 router.get('/note/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
     // if id exists, find by id
-    // else res.send(404);
-
+    if(result) {
+        res.json(result)
+    }
+    else {
+        res.sendStatus(404);
+    }
 });
 
 // post route
 router.post('/notes', (req, res) => {
     // set random id to each new note
-    req.body.id = Math.floor((Math.random()*1000000000000000));
+    req.body.id = (Math.floor((Math.random()*1000000000000000))).toString();
     
-    console.log(req.body);
-    res.json({
-        message: 'Note created.',
-        id: req.body.id
-    })
+    // add note to db.json and notes array 
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
 });
 
 // // delete route
